dfe7406599a9d7467043392ef4549362fdd2718a
tahaseefshaik <tahaseefshaik@gmail.com>
2017-10-19 22:17:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tahas/loginscreen/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.tahas.loginscreen;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative_layout);
+    }
+
+
+    }
+
+
